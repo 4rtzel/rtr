8468bb99281256fb0a6a9d2cba6b89c654f98b03
4rtzel <4rtzel@gmail.com>
2021-05-24 23:18:12 +0300

    Add a new SplitLines type. Change the input and output types of grid_slice_parser.
    SplitLines type is used as a way to provide an interator to splitted lines (Vec<String>) from different sources (file and stdin).
    
    grid_slice_parser now accepts an iterator to Vec<String> and returns an iterator that generates Vec<String> as well. This should allow chaining different features together with a simple iterator interface (e.g. GridSlice applied to the lines that were already filtered by GridSlice).

--- src/grid_slice.rs
+++ src/grid_slice.rs
@@ -4,7 +4,7 @@ use crate::parser::grid_slice_parser;
 
 enum GridSliceSource<I> {
     Iter(I),
-    SavedLines(Vec<io::Result<String>>),
+    SavedLines(Vec<Vec<String>>),
 }
 
 pub struct GridSlice<I> {
@@ -13,20 +13,17 @@ pub struct GridSlice<I> {
     num_line: usize,
 }
 
-impl<I: Iterator<Item = io::Result<String>>> Iterator for GridSlice<I> {
-    type Item = String;
+impl<I: Iterator<Item = Vec<String>>> Iterator for GridSlice<I> {
+    type Item = Vec<String>;
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            let line = match self.source {
-                GridSliceSource::Iter(ref mut i) => i.next()?.ok()?,
-                GridSliceSource::SavedLines(ref l) => {
-                    l.get(self.num_line)?.as_ref().ok()?.to_string()
-                }
+            let words: Vec<String> = match self.source {
+                GridSliceSource::Iter(ref mut i) => i.next()?,
+                GridSliceSource::SavedLines(ref l) => l.get(self.num_line)?.to_vec(),
             };
             self.num_line += 1;
             if is_inside_range(&self.grid_slice.line, self.num_line as i64 - 1) {
-                let words: Vec<&str> = line.split_whitespace().collect();
                 let field_range = normalize_range(&self.grid_slice.field, words.len());
                 if field_range.step > 0 {
                     return Some(filter_line(&field_range, words.into_iter()));
@@ -38,7 +35,7 @@ impl<I: Iterator<Item = io::Result<String>>> Iterator for GridSlice<I> {
     }
 }
 
-pub fn grid_slice_iter<I: Iterator<Item = io::Result<String>>>(
+pub fn grid_slice_iter<I: Iterator<Item = Vec<String>>>(
     mut grid_slice: grid_slice_parser::GridSliceFilter,
     iter: I,
 ) -> GridSlice<I> {
@@ -46,7 +43,7 @@ pub fn grid_slice_iter<I: Iterator<Item = io::Result<String>>>(
         grid_slice.line.from < 0 || grid_slice.line.to < -1 || grid_slice.line.step < 0;
 
     if post_process {
-        let mut lines: Vec<Result<String, std::io::Error>> = iter.collect();
+        let mut lines: Vec<Vec<String>> = iter.collect();
         grid_slice.line = normalize_range(&grid_slice.line, lines.len());
         if grid_slice.line.step < 0 {
             lines.reverse();
@@ -100,13 +97,12 @@ fn is_inside_range(range: &grid_slice_parser::GridSliceRange, current: i64) -> b
         && (((range.from - current) % range.step) == 0)
 }
 
-fn filter_line<'a, I: Iterator<Item = &'a str>>(
+fn filter_line<I: Iterator<Item = String>>(
     range: &grid_slice_parser::GridSliceRange,
     word: I,
-) -> String {
+) -> Vec<String> {
     word.enumerate()
         .filter(|(n, _)| is_inside_range(range, *n as i64))
         .map(|(_, w)| w)
-        .collect::<Vec<&str>>()
-        .join(" ")
+        .collect::<Vec<String>>()
 }
--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,29 @@ fn parse_args() -> Result<Opts, &'static str> {
     })
 }
 
+struct SplitLines<I: BufRead> {
+    source: I,
+}
+
+impl<I: BufRead> SplitLines<I> {
+    fn new(source: I) -> Self {
+        SplitLines { source: source }
+    }
+}
+
+impl<I: BufRead> Iterator for SplitLines<I> {
+    type Item = Vec<String>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut line = String::new();
+        if self.source.read_line(&mut line).ok()? == 0 {
+            None
+        } else {
+            Some(line.split_whitespace().map(|f| f.to_string()).collect())
+        }
+    }
+}
+
 fn main() -> Result<(), &'static str> {
     let opts = parse_args()?;
     let grid_slice = parser::grid_slice_parser::parse_grid_slice(&opts.program)?;
@@ -31,14 +54,14 @@ fn main() -> Result<(), &'static str> {
             let file = File::open(f).or(Err("Unable to open a file"))?;
             let reader = BufReader::new(file);
 
-            for line in grid_slice::grid_slice_iter(grid_slice, reader.lines()) {
-                println!("{}", line);
+            for line in grid_slice::grid_slice_iter(grid_slice, SplitLines::new(reader)) {
+                println!("{}", line.join(" "));
             }
         }
         None => {
             let stdin = io::stdin();
-            for line in grid_slice::grid_slice_iter(grid_slice, stdin.lock().lines()) {
-                println!("{}", line);
+            for line in grid_slice::grid_slice_iter(grid_slice, SplitLines::new(stdin.lock())) {
+                println!("{}", line.join(" "));
             }
         }
     }
